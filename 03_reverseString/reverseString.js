44a5e9fe8937fc314ff10e07c30563d40619262a
Edu Schweitzer <edu.schweitzerr@gmail.com>
2022-07-07 18:47:59 -0300

    Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split('');
+    let finalWord = ''
+    let reverseWord = wordArray.reverse(); 
+    return reverseWord.join('');
 };
 
 // Do not edit below this line
