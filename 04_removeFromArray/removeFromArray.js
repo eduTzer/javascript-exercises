be88e4a79ae0a854847b4f0fc0f72c3e392de7f2
Edu Schweitzer <edu.schweitzerr@gmail.com>
2022-07-09 12:35:12 -0300

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeElement) {
+    let removeArray = [...removeElement];
+    for (let i = 0; i < removeArray.length; i++){
+        let removeVar = removeArray[i];
+        for (let j = 0; j < array.length; j++){
+            if (removeVar === array[j]){
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
