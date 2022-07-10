925fb1d4031c0d2b1deb3556021102fdfa0ebe2c
Edu Schweitzer <edu.schweitzerr@gmail.com>
2022-07-10 12:45:50 -0300

    Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(var1, var2) {
+    let totalSum;
+    if (var1 <= 0 || var2 <= 0 || typeof(var1) !== 'number' || typeof(var2) !== 'number'){
+        return 'ERROR'
+    } else{
+        let numberElements = var1 - var2;
+        if (numberElements < 0){
+            totalSum = (var1 + var2)*(-numberElements + 1)/2;
+        }else{
+            totalSum = (var1 + var2)*(numberElements + 1)/2;
+        }
+    }
+    
+    return totalSum;
 };
 
 // Do not edit below this line
